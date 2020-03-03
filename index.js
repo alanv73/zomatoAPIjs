b0e702e1696a7f296248f383b6f6422ae4d1206f
Alan Van Art <alanv73@gmail.com>
2020-03-03 09:31:35 -0500

    move keys to keys.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-
-const zipApiKey = 'js-dnTQcPjRZVp1iBlDpEt7W4JfRwHrHOdqkFgBFUK7Q53zP5oA9cYkMCXey2ZGCDMU';
-const googleGeoApi = 'AIzaSyBe-Piv9VcT8gCXPp8bMEHdeUSgMxPV4Xw';
-const zomatoKey = '47dd3222c21b713ce2e5eb66331f2f1b';
 let hasGmap = false;
 let restaurantList = [];
 let cityLat;
